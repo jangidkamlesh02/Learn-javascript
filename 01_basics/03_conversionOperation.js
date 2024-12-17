4997e331b33cca255a0455e047807fec6ef34160
jangidkamlesh02 <kamleshjadwal12@gmail.com>
2024-12-17 15:49:09 +0530

    Add files via upload

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -70,5 +70,6 @@ console.log(gameCounter);
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 // Nan // Not a Number
+
 // Terminal Command
 // node 01_basics/03_conversionOperation.js
---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,54 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 2);
+console.log(2 == 2);
+console.log(2 != 2);
+
+// ************** Don't use this Comparison of datatypes start *************
+
+console.log("2" > 2);
+console.log("02" > 2);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ************** Don't use this Comparison of datatypes End *************
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+// node 01_basics/04_Comparison.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Terminal Command
+// node 01_basics/04_Comparison.js
