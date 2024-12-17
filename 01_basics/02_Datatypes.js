af81da50105891ff29e289f97e2706e62a91fba3
jangidkamlesh02 <kamleshjadwal12@gmail.com>
2024-12-17 15:50:25 +0530

    Add files via upload

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -4,9 +4,9 @@
 
 console.log(3 + 3); // code readability should be high
 
-console.log("Hitesh");
+console.log("kamlesh");
 
-let name = "hitesh";
+let name = "kamlesh";
 let age = 18;
 let isLoggedIn = false;
 let state;
